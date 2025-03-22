a7ba2fdf600bb988b957a315d7d975c488f68e24
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-03-22 12:13:17 +0530

    Model analyzer service update for tempfile analysis

--- sound_analysis_controller.py
+++ sound_analysis_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
 from sound_analysis_services import analyze_recording
 
@@ -7,5 +7,9 @@ sound_analysis = Blueprint('sound_analysis', __name__, url_prefix='/api')
 @sound_analysis.route('/sound_analysis', methods=['POST'])
 @cross_origin()
 def analyze_sound():
-    params = request.get_json()
-    return analyze_recording(params)
+    if 'wavfile' not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+    
+    wav_file = request.files['wavfile']
+    result = analyze_recording(wav_file)
+    return jsonify(result)
