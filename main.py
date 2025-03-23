afa52a47a7449e477166f5ed4c54ba62acdbb42a
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-03-23 10:57:26 +0530

    Services and controller update

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 import os
+from routes import register_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(os.environ['CONFIG_TYPE'])
+    config_type = os.environ.get('CONFIG_TYPE', 'config.DevelopmentConfig')
+    app.config.from_object(config_type)
     app = register_routes(app)
 
     return app
 
-app = Flask(__name__)
-app = register_routes(app)
 if __name__ == "__main__":
+    app = create_app()
     # app.run(port=5001, debug=True)
     app.run(host="0.0.0.0", port=5001, debug=True)
